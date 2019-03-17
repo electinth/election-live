d088ede37d2eeab0939312e07e59267158e81345
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-03-18 02:02:41 +0700

    Adjust <PartyStatsRow /> styling with <VisuallyHidden> for screen readers

--- src/components/PartyStatsRow.js
+++ src/components/PartyStatsRow.js
@@ -1,16 +1,36 @@
 import React from "react"
+import VisuallyHidden from "@reach/visually-hidden"
 export function PartyStatsRow() {
   // @todo #12 Make <PartyStatsRow /> accept props and display values based on props.
 
-  // @todo #12 Adjust <PartyStatsRow /> styling according to design.
+  // @todo #14 Implement score bar in <PartyStatsRow />.
 
   return (
-    <article css={{ display: "flex" }}>
-      <h3>ชื่อพรรค</h3>
-      <p>
-        ทั้งหมด 70 ที่นั่ง (14%) สส.เขต 20 ที่นั่ง สส.ปาร์ตี้ลิสต์ 50 ที่นั่ง
+    <article css={{ position: "relative", height: 96, padding: "0.5rem" }}>
+      <h3 css={{ margin: 0, fontSize: "1.5rem" }}>ชื่อพรรค</h3>
+      <p css={{ margin: 0 }}>
+        <VisuallyHidden>ทั้งหมด </VisuallyHidden>
+        <span
+          css={{
+            position: "absolute",
+            right: "0.5rem",
+            top: 0,
+            fontSize: "2rem",
+          }}
+        >
+          70
+        </span>
+        <VisuallyHidden> ที่นั่ง (</VisuallyHidden>
+        <span css={{ position: "absolute", right: "0.5rem", top: 45 }}>
+          14%
+        </span>
+        <VisuallyHidden>) </VisuallyHidden>
       </p>
-      <p>ต้องการอีก 50 เพื่อเป็นอันดับหนึ่ง</p>
+      <p css={{ margin: 0 }}>ต้องการอีก 50 เพื่อเป็นอันดับหนึ่ง</p>
+      <p css={{ margin: 0 }}>
+        สส.เขต 20 <VisuallyHidden> ที่นั่ง </VisuallyHidden>
+        สส.ปาร์ตี้ลิสต์ 50 <VisuallyHidden> ที่นั่ง</VisuallyHidden>
+      </p>
     </article>
   )
 }
