0662993432a956e182d183ee5414c6b9ca554220
dodadoa <maddodadoa@gmail.com>
2019-03-18 04:22:34 +0700

    add/setup unit test to the project and add sample test on navbar

---
+++ src/components/__tests__/NavBar.test.js
@@ -0,0 +1,21 @@
+import React from "react"
+import TestRenderer from "react-test-renderer"
+import NavBar from "../NavBar"
+import { Link } from "gatsby"
+
+// example test. might not need.
+describe("Navbar", () => {
+
+  let renderer
+
+  beforeEach(() => {
+    renderer = TestRenderer.create(<NavBar />)
+  })
+
+  test('should have 4 Links', () => {
+    const root = renderer.root
+    console.log(renderer.toJSON())
+    const links = root.findAll((el) => el.type === Link)
+    expect(links).toHaveLength(4)
+  })
+})
