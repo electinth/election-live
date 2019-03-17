52d8590c6a770be88969d210a0c4fe2974919906
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-03-18 00:58:30 +0700

    Add TODO about Progressive Web App manifest file

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,14 @@ module.exports = {
         },
       },
     },
+
     // @todo #1 Add Analytics, e.g. Google Analytics.
     //  Check out Gatsby docs for how to add analytics
     //  - Main Guide: https://www.gatsbyjs.org/docs/adding-analytics/
     //  - Google Analytics: https://www.gatsbyjs.org/docs/adding-analytics/#using-gatsby-plugin-google-analytics
     //  - Google Tag Manager: https://www.gatsbyjs.org/packages/gatsby-plugin-google-tagmanager/
+
+    // @todo #1 Add PWA manifest file using gatsby-plugin-manifest.
+    //  (see: https://www.gatsbyjs.org/docs/add-a-manifest-file/)
   ],
 }
