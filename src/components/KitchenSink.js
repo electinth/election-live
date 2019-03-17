dc21a7fedeb2d3f22efd692b527718fe2bae93c4
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-03-18 01:23:33 +0700

    Add kitchen sink

---
+++ src/components/KitchenSink.js
@@ -0,0 +1,38 @@
+import React from "react"
+
+export function KitchenSink() {
+  return (
+    <section>
+      <h1>Kitchen Sink</h1>
+      <h2>Vote</h2>
+      <Example maxWidth={375}>
+        <PartyStatsRow />
+      </Example>
+    </section>
+  )
+}
+
+function PartyStatsRow() {
+  // @todo #1 Implement <PartyStatsRow /> component
+  return "TODO"
+}
+
+function Example({ maxWidth, children }) {
+  return (
+    <div
+      css={{
+        display: "flex",
+        width: "100%",
+        maxWidth: maxWidth + 8,
+        padding: "4px 0",
+        justifyContent: "center",
+        background: "#ccc",
+        verticalAlign: "top",
+      }}
+    >
+      <div css={{ background: "white", flex: 1, maxWidth: maxWidth }}>
+        {children}
+      </div>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
 import Layout from "../components/layout"
+import { KitchenSink } from "../components/KitchenSink"
 
-export default () => (
+export default ({ pageContext }) => (
   <Layout>
     <h1>Map View</h1>
+    <KitchenSink />
   </Layout>
 )
