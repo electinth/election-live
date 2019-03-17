5bc8ee2b5a752184b35831ed7578c0a32a4304e8
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-03-18 00:39:36 +0700

    Add TODO for analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [
-    'gatsby-plugin-emotion',
+    "gatsby-plugin-emotion",
     {
       resolve: "gatsby-plugin-web-font-loader",
       options: {
@@ -9,5 +9,10 @@ module.exports = {
         },
       },
     },
+    // @todo #1 Add Analytics, e.g. Google Analytics.
+    //  Check out Gatsby docs for how to add analytics
+    //  - Main Guide: https://www.gatsbyjs.org/docs/adding-analytics/
+    //  - Google Analytics: https://www.gatsbyjs.org/docs/adding-analytics/#using-gatsby-plugin-google-analytics
+    //  - Google Tag Manager: https://www.gatsbyjs.org/packages/gatsby-plugin-google-tagmanager/
   ],
 }
