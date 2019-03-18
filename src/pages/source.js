216b31feb25ab37e8ac262064610e926eb73b4ec
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-03-18 21:11:03 +0700

    Add TODO for source page

--- src/pages/source.js
+++ src/pages/source.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 
+// @todo #1 Add content about where we got the data from
 export default () => (
   <Layout>
     <h1>Data Source</h1>
