ad55012c27bee4e8e139efbb2bfbbe60e6b249b2
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-03-19 20:27:45 +0700

    Add development mode disclaimer

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -1,9 +1,10 @@
-import React from "react"
+import React, { useState, useReducer, useEffect } from "react"
 import GlobalPanel from "./GlobalPanel"
 import NavBar from "./NavBar"
 import Footer from "./Footer"
 import { PageContentContextProvider, PageContentOutlet } from "./PageContent"
 import { GlobalStyle } from "../styles"
+import { Link } from "gatsby"
 
 // @todo #1 Implement responsive layout and navigation
 
@@ -14,6 +15,7 @@ export default function PageLayout({ children }) {
   return (
     <PageContentContextProvider>
       <GlobalStyle />
+      <DevModeDisclaimer />
       <div css={{ margin: "13px 16px" }}>
         <img
           src={require("../styles/images/site-logo.png")}
@@ -39,5 +41,43 @@ export default function PageLayout({ children }) {
       {children}
       <Footer />
     </PageContentContextProvider>
+  )
+}
+
+function DevModeDisclaimer() {
+  const [disclaimerHidden, hideDisclaimer] = useReducer(() => true, false)
+  useEffect(() => {
+    const t = setTimeout(() => hideDisclaimer(), 2000)
+    return () => clearTimeout(t)
+  }, [])
+  return (
+    <div
+      data-show-disclaimer={disclaimerHidden ? undefined : true}
+      css={{
+        position: "absolute",
+        background: "#F0324B",
+        color: "#fff",
+        top: 5,
+        left: 5,
+        transform: "rotate(-10deg)",
+        padding: "5px 10px",
+        opacity: 0,
+        transition: "0.5s opacity",
+        "&:hover, &[data-show-disclaimer]": {
+          opacity: 1,
+        },
+      }}
+    >
+      <strong>Development mode</strong>
+      <br />
+      Using mock data
+      <br />
+      <Link
+        to="/dev/kitchen-sink"
+        css={{ color: "#ff0", textDecoration: "none" }}
+      >
+        &rarr; Go to kitchen sink!
+      </Link>
+    </div>
   )
 }
