d151339a7a17930bf8ba2b8db510966f58ddf6ba
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-03-21 01:28:22 +0700

    Extract PartyStats generation logic to partyStatsFromSummaryJSON

--- src/models/PartyStats.js
+++ src/models/PartyStats.js
@@ -1,3 +1,7 @@
+import _ from "lodash"
+import { parties, getPartyById } from "./information"
+import { calculatePartyList } from "thailand-party-list-calculator"
+
 /**
  * Summarizes the number of constituency seats and party list seats of each party.
  * @typedef {PartyStatsRow[]} PartyStats
@@ -9,6 +13,43 @@
  * @prop {number} constituencySeats
  * @prop {number} partyListSeats
  */
+
+/**
+ * @param {ElectionDataSource.SummaryJSON} summary
+ */
+export function partyStatsFromSummaryJSON(summary) {
+  // Calculate the constituency seat count for each party.
+  const constituencySeatCount = _(summary.zoneWinningCandidateMap)
+    .values()
+    .flatMap(z => _.values(z))
+    .countBy(c => c.partyId)
+    .value()
+
+  // Calculate seat counts for ecah party.
+  const partyStats = _(parties)
+    .map(party => {
+      return {
+        id: party.id,
+        electedMemberCount: constituencySeatCount[party.id] || 0,
+        voteCount: summary.partyScoreMap[party.id] || 0,
+        partyListCandidateCount: party.partylist,
+      }
+    })
+    .thru(calculatePartyList)
+    .map(calculated => {
+      const partyId = calculated.id
+      const party = getPartyById(partyId)
+      return {
+        party,
+        constituencySeats: calculated.electedMemberCount,
+        partyListSeats: calculated.partyListMemberCount,
+      }
+    })
+    .sortBy(row => row.constituencySeats + row.partyListSeats)
+    .reverse()
+    .value()
+  return partyStats
+}
 
 /**
  * @param {PartyStats} partyStats
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,7 @@ import ZoneMasterView from "../components/ZoneMasterView"
 import NationwideSummaryHeader from "../components/NationwideSummaryHeader"
 import PartyStatsList from "../components/PartyStatsList"
 import { useSummaryData } from "../models/LiveDataSubscription"
-import { parties, getPartyById } from "../models/information"
-import { calculatePartyList } from "thailand-party-list-calculator"
+import { partyStatsFromSummaryJSON } from "../models/partyStats"
 
 export default ({ pageContext }) => (
   <MainLayout>
@@ -45,39 +44,6 @@ function NationwidePartyStatsContainer() {
   const summaryState = useSummaryData()
   if (summaryState.loading) return null
   const summary = summaryState.data
-
-  // @todo #53 [Refactor] Move party stats calculation logic to models/PartyStats.
-
-  // Calculate the constituency seat count for each party.
-  const constituencySeatCount = _(summary.zoneWinningCandidateMap)
-    .values()
-    .flatMap(z => _.values(z))
-    .countBy(c => c.partyId)
-    .value()
-
-  // Calculate seat counts for ecah party.
-  const partyStats = _(parties)
-    .map(party => {
-      return {
-        id: party.id,
-        electedMemberCount: constituencySeatCount[party.id] || 0,
-        voteCount: summary.partyScoreMap[party.id] || 0,
-        partyListCandidateCount: party.partylist,
-      }
-    })
-    .thru(calculatePartyList)
-    .map(calculated => {
-      const partyId = calculated.id
-      const party = getPartyById(partyId)
-      return {
-        party,
-        constituencySeats: calculated.electedMemberCount,
-        partyListSeats: calculated.partyListMemberCount,
-      }
-    })
-    .sortBy(row => row.constituencySeats + row.partyListSeats)
-    .reverse()
-    .value()
-
+  const partyStats = partyStatsFromSummaryJSON(summary)
   return <PartyStatsList partyStats={partyStats} />
 }
