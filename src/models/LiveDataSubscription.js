7adb21ab1f6ceae6bedd5b389d027d7072052005
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-03-21 03:32:46 +0700

    Add some mock loading state

--- src/models/LiveDataSubscription.js
+++ src/models/LiveDataSubscription.js
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react"
+
 /** @return {{ loading: boolean, data: ElectionDataSource.SummaryJSON }} */
 export function useSummaryData() {
   // @todo #52 Replace mock data with real data loading logic.
-  return {
-    loading: false,
-    data: require("./__fixtures__/Summary1.json"),
-  }
+  const [state, setState] = useState({
+    loading: true,
+    data: null,
+  })
+  useEffect(() => {
+    setTimeout(() => {
+      import("./__fixtures__/Summary1.json").then(result => {
+        console.log(result)
+        setState({
+          loading: false,
+          data: result,
+        })
+      })
+    }, 1000)
+  }, [])
+  return state
 }
