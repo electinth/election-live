cf1303adaa5049057e0cb41ae6d8dad1f489bff7
heytitle <pat.chormai@gmail.com>
2019-03-22 00:16:58 +0700

    style NationwideSummaryHeader

--- src/components/NationwideSummaryHeader.js
+++ src/components/NationwideSummaryHeader.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { DISPLAY_FONT, labelColor } from "../styles"
 
 // @ts-check
 
@@ -36,14 +37,94 @@ export default function NationwideSummaryHeader(props) {
     percentage
   ) {
     return (
-      <div>
-        <h1>{title}</h1>
-        <p>เขตทั้งหมด {totalZoneCount} เขต</p>
-        <p>นับเสร็จแล้ว {completedZoneCount} เขต</p>
-        <p>
-          มีผู้มาใช้สิทธิ์ {totalVoteCount} คน ({percentage}%)
+      <div css={{ textAlign: "center" }}>
+        <h1 css={{ fontFamily: DISPLAY_FONT, borderBottom: "1px solid" }}>
+          {title}
+        </h1>
+        <div css={{ borderBottom: "1px solid", paddingBottom: 10 }}>
+          {buildNationwideSubSummaryHeader("เขตทั้งหมด", totalZoneCount, 0)}
+          {buildNationwideSubSummaryHeader(
+            "นับเสร็จแล้ว",
+            completedZoneCount,
+            1
+          )}
+        </div>
+        <p
+          css={{
+            textAlign: "left",
+            color: labelColor,
+            fontSize: "1.1em",
+            borderBottom: "1px solid black",
+            paddingBottom: 10,
+          }}
+        >
+          มีผู้มาใช้สิทธิ์
+          <span
+            css={{
+              fontSize: "1.5rem",
+              fontFamily: DISPLAY_FONT,
+              marginLeft: 30,
+              marginRight: 10,
+              color: "black",
+            }}
+          >
+            {totalVoteCount}
+          </span>
+          คน
+          <span
+            css={{
+              fontSize: "1.5rem",
+              fontFamily: DISPLAY_FONT,
+              position: "absolute",
+              right: 0,
+              color: "black",
+            }}
+          >
+            {percentage}%
+          </span>
         </p>
       </div>
+    )
+  }
+
+  function buildNationwideSubSummaryHeader(label, stat, idx) {
+    return (
+      <span
+        css={{
+          width: "49%",
+          display: "inline-block",
+        }}
+      >
+        <div
+          css={{
+            paddingRight: idx == 0 ? "0.5rem" : 0,
+            paddingLeft: idx == 1 ? "0.5rem" : 0,
+            borderRight: idx == 0 ? "1px solid black" : "none",
+          }}
+        >
+          <span
+            css={{
+              fontSize: "1.1em",
+              color: labelColor,
+              float: "left",
+              marginTop: ".5em",
+            }}
+          >
+            {" "}
+            {label}
+          </span>
+          <span
+            css={{
+              fontFamily: DISPLAY_FONT,
+              fontSize: "1.5rem",
+              float: "right",
+            }}
+          >
+            {stat}
+          </span>
+          <div css={{ clear: "both" }} />
+        </div>
+      </span>
     )
   }
 }
