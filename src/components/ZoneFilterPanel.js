104233d1d7ecb5c704a9c2a501b3d829222b2cb7
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-03-21 23:21:33 +0700

    Update ZoneFilterPanel based on ZoneMasterView

--- src/components/ZoneFilterPanel.js
+++ src/components/ZoneFilterPanel.js
@@ -1,28 +1,46 @@
-import React, { createContext } from "react"
-import Placeholder from "./Placeholder"
-import { filters, filterPath } from "../models/information"
+import React, { createContext, useState } from "react"
+import {
+  filters as areaFilters,
+  filterPath,
+  zonesForSearch,
+} from "../models/information"
 import { Link } from "gatsby"
+import { labelColor } from "../styles"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCheckCircle, faCircle } from "@fortawesome/free-regular-svg-icons"
+import { faSearch, faTimes } from "@fortawesome/free-solid-svg-icons"
 
 const ZoneFilterContext = createContext(/** @type {ZoneFilterName} */ ("all"))
 
 export const ZoneFilterContextProvider = ZoneFilterContext.Provider
 
 export function ZoneFilterPanel() {
+  const [state, setState] = useState({
+    isSearchOpen: false,
+    isFilterOpen: false,
+    zoneQuery: null,
+  })
   // @todo #1 Create a 1st version of ZoneFilterPanel component
   //  in place of the Placeholder. May be a dummy component.
   return (
-    <Placeholder height={400}>
-      {renderFilter("all")}
-      {renderFilter("northern")}
-      {renderFilter("northeastern")}
-      {renderFilter("central")}
-      {renderFilter("southern")}
-      <hr />
-      {renderFilter("urban")}
-      {renderFilter("rural")}
-      {renderFilter("gerrymandering")}
-      {renderFilter("swing")}
-    </Placeholder>
+    <div>
+      <div css={{ color: labelColor, fontWeight: 600 }}>ตัวเลือกแสดงผล</div>
+      <div css={{ fontSize: 20, fontWeight: "bold" }}>เขตพื้นที่</div>
+      <ul css={{ padding: 0, listStyle: "none" }}>
+        {renderFilter("all")}
+        {renderFilter("northern")}
+        {renderFilter("northeastern")}
+        {renderFilter("central")}
+        {renderFilter("southern")}
+      </ul>
+      <div css={{ fontSize: 20, fontWeight: "bold" }}>ตัวเลือกพิเศษ</div>
+      <ul css={{ padding: 0, listStyle: "none" }}>
+        {renderFilter("urban")}
+        {renderFilter("rural")}
+        {renderFilter("gerrymandering")}
+        {renderFilter("swing")}
+      </ul>
+    </div>
   )
 
   /**
@@ -32,17 +50,28 @@ export function ZoneFilterPanel() {
     return (
       <div>
         <ZoneFilterContext.Consumer>
-          {currentFilterName => (
-            <Link
-              css={{
-                fontWeight:
-                  currentFilterName === filterName ? "bold" : "normal",
-              }}
-              to={filterPath(filterName)}
-            >
-              {filters[filterName].name.th}
-            </Link>
-          )}
+          {currentFilterName => {
+            const current = currentFilterName === filterName
+            return (
+              <li css={{ paddingTop: 10, paddingBottom: 10 }}>
+                <Link
+                  to={filterPath(filterName)}
+                  css={{
+                    opacity: current ? 1 : 0.62,
+                    display: "block",
+                    textDecoration: "none",
+                    color: "inherit",
+                  }}
+                >
+                  <span css={{ marginRight: 10 }}>
+                    {current && <FontAwesomeIcon icon={faCheckCircle} />}
+                    {!current && <FontAwesomeIcon icon={faCircle} />}
+                  </span>
+                  <span>{areaFilters[filterName].name.th}</span>
+                </Link>
+              </li>
+            )
+          }}
         </ZoneFilterContext.Consumer>
       </div>
     )
