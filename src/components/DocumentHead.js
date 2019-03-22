fb96f145cc50e5beb78dd5e60e0940186a36aeb4
heytitle <pat.chormai@gmail.com>
2019-03-22 16:34:51 +0700

    add favicon

--- src/components/DocumentHead.js
+++ src/components/DocumentHead.js
@@ -3,7 +3,15 @@ import { Helmet } from "react-helmet"
 
 export default function DocumentHead() {
   return (
-    <Helmet>
+    <Helmet
+      link={[
+        {
+          rel: "shortcut icon",
+          type: "image/x-icon",
+          href: "https://elect.in.th/wp-content/uploads/2018/10/favicon.ico",
+        },
+      ]}
+    >
       <title>ELECT LIVE</title>
     </Helmet>
   )
