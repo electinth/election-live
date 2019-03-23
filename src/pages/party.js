89068f4759ed803002572695f8e38af0dee341a5
Pankamol Srikaew <aim.ske09@gmail.com>
2019-03-23 18:37:24 +0700

    remove unused import

--- src/pages/party.js
+++ src/pages/party.js
@@ -1,19 +1,5 @@
 import React from "react"
 import MainLayout from "../components/MainLayout"
-import {
-  parties,
-  partyPath,
-  getPartyById,
-  partyColor,
-} from "../models/information"
-
-import {
-  buttonStyle,
-  DESKTOP_MIN_WIDTH,
-  labelColor,
-  media,
-  Responsive,
-} from "../styles"
 import ZonePartyView from "../components/ZonePartyView"
 
 export default ({ pageContext }) => (
