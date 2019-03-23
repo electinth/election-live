3152849242e748c8be57586b4bb44c3f70ab4aec
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-03-24 03:33:59 +0700

    Amplitude requires window aah

--- src/util/analytics.js
+++ src/util/analytics.js
@@ -1,11 +1,16 @@
-import amplitude from "amplitude-js"
 import { Debug } from "./Debug"
 
 const debug = Debug("elect:analytics")
 
 export function trackEvent(eventName, eventProperties) {
   if (process.env.NODE_ENV === "production") {
-    amplitude.getInstance().logEvent(eventName, eventProperties)
+    try {
+      const amplitude = require("amplitude-js")
+      amplitude.getInstance().logEvent(eventName, eventProperties)
+    } catch (e) {
+      debug(eventName, eventProperties)
+      debug("Failed to send event to Amplitude", e)
+    }
   } else {
     debug(eventName, eventProperties)
   }
