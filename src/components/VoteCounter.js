cf389a012dcb2b33677ccebeb6e7875518c5f84d
Pankamol Srikaew <aim.ske09@gmail.com>
2019-03-23 16:47:35 +0700

    add lastupdate

--- src/components/VoteCounter.js
+++ src/components/VoteCounter.js
@@ -6,7 +6,6 @@ import { Responsive } from "../styles"
 
 export default function VoteCounter({ percentage = 60, lastUpdate = "23:59" }) {
   // @todo #1 inject vote count props to this comp for showing total vote %
-  const { percentage, lastUpdate } = props
 
   return (
     <div
