c4818c173037b8e53c4b9cd09e44c12ba059fce5
Pankamol Srikaew <aim.ske09@gmail.com>
2019-03-23 18:25:39 +0700

    add party viewa

---
+++ src/components/ZonePartyView.js
@@ -0,0 +1,135 @@
+import React from "react"
+import {
+  parties,
+  partyPath,
+  getPartyById,
+  partyColor,
+} from "../models/information"
+import { DESKTOP_MIN_WIDTH, labelColor, media } from "../styles"
+import { faSearch } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import ErrorBoundary from "../components/ErrorBoundary"
+import ElectionMapContainer from "../components/ElectionMapContainer"
+
+export default function ZonePartyView() {
+  return (
+    <div>
+      <div
+        css={{
+          margin: "0 auto",
+          padding: "0 24px",
+          maxWidth: "1200px",
+          [media(DESKTOP_MIN_WIDTH)]: {
+            display: "block",
+            order: 1,
+            margin: "0 0 10px",
+            padding: 0,
+            display: "flex",
+          },
+        }}
+      >
+        <ZoneSearchParty />
+        <ZoneMapView />
+        <ZoneVoteResult />
+      </div>
+    </div>
+  )
+}
+
+function ZoneSearchParty() {
+  return (
+    <div
+      css={{
+        [media(DESKTOP_MIN_WIDTH)]: {
+          display: "block",
+          order: 1,
+          margin: "0 0 10px",
+          padding: 0,
+        },
+      }}
+    >
+      {/* left zone full mobile*/}
+      <div css={{ position: "relative" }}>
+        <input
+          css={{
+            border: `1px solid ${labelColor}`,
+            width: "100%",
+            boxSizing: "border-box",
+            padding: 10,
+            fontSize: 16,
+            marginTop: 20,
+            ["&:focus"]: { outline: 0 },
+          }}
+          placeholder="ชื่อพรรคการเมือง"
+          onChange={v => {
+            // @todo #1 search party: implement onChange
+          }}
+        />
+        <div
+          css={{
+            top: 30,
+            position: "absolute",
+            right: 10,
+            color: labelColor,
+          }}
+        >
+          <FontAwesomeIcon icon={faSearch} />
+        </div>
+      </div>
+      <div>{/* zone list of party */}</div>
+    </div>
+  )
+}
+
+function ZoneMapView() {
+  return (
+    <div
+      css={{
+        [media(DESKTOP_MIN_WIDTH)]: {
+          display: "block",
+          order: 2,
+          margin: "10px auto",
+          padding: 0,
+          width: "375px",
+        },
+      }}
+    >
+      {/* map zone */}
+      <div
+        css={{
+          // display: currentMobileTab === "map" ? "block" : "none",
+          display: "block",
+          margin: "10px auto",
+          width: 375,
+          [media(DESKTOP_MIN_WIDTH)]: {
+            display: "block",
+            order: 2,
+          },
+        }}
+      >
+        <ErrorBoundary name="ElectionMap">
+          <ElectionMapContainer />
+        </ErrorBoundary>
+      </div>
+    </div>
+  )
+}
+
+function ZoneVoteResult() {
+  return (
+    <div
+      css={{
+        [media(DESKTOP_MIN_WIDTH)]: {
+          display: "block",
+          order: 3,
+          margin: "0",
+          padding: "16px",
+          width: "320px",
+        },
+      }}
+    >
+      {/* right zone */}
+      <div>ประมาณจำนวน สส. ที่ได้</div>
+    </div>
+  )
+}
--- src/pages/party.js
+++ src/pages/party.js
@@ -7,24 +7,40 @@ import {
   partyColor,
 } from "../models/information"
 import { Link } from "gatsby"
+import {
+  buttonStyle,
+  DESKTOP_MIN_WIDTH,
+  labelColor,
+  media,
+  Responsive,
+} from "../styles"
+import ZonePartyView from "../components/ZonePartyView"
 
 export default ({ pageContext }) => (
   <MainLayout activeNavBarSection="by-party">
-    <h1>Party View</h1>
-    {pageContext.partyId ? (
-      <h2>{getPartyById(pageContext.partyId).name}</h2>
-    ) : (
-      <h2>All parties</h2>
-    )}
-
-    <ul>
-      {parties.map(p => (
-        <li key={p.id}>
-          <Link to={partyPath(p)} style={{ color: partyColor(p) }}>
-            {p.name}
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <ZonePartyView />
   </MainLayout>
 )
+
+function PartyList({ pageContext }) {
+  return (
+    <div>
+      <h1>Party View</h1>
+      {pageContext.partyId ? (
+        <h2>{getPartyById(pageContext.partyId).name}</h2>
+      ) : (
+        <h2>All parties</h2>
+      )}
+
+      <ul>
+        {parties.map(p => (
+          <li key={p.id}>
+            <Link to={partyPath(p)} style={{ color: partyColor(p) }}>
+              {p.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
