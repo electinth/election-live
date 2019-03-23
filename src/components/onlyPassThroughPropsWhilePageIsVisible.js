7fe1f7a581eed5de1948633879c78f48e5b47ff4
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-03-23 18:45:39 +0700

    แค่ require ก็พังแล้วหรอ?

--- src/components/onlyPassThroughPropsWhilePageIsVisible.js
+++ src/components/onlyPassThroughPropsWhilePageIsVisible.js
@@ -1,19 +1,19 @@
 import React from "react"
-import Visibility from "visibilityjs"
 
 export default function onlyPassThroughPropsWhilePageIsVisible(BaseComponent) {
   return class UpdateBlocker extends React.PureComponent {
     componentWillUnmount() {
       if (this.listener) {
+        const Visibility = require("visibilityjs")
         Visibility.unbind(this.listener)
       }
     }
     shouldComponentUpdate() {
+      const Visibility = require("visibilityjs")
       if (Visibility.isSupported() && Visibility.hidden()) {
         if (!this.listener) {
           this.listener = Visibility.onVisible(() => {
             requestAnimationFrame(() => {
-              console.log("Ok resuming updates")
               this.forceUpdate()
               if (this.listener) {
                 Visibility.unbind(this.listener)
@@ -22,7 +22,6 @@ export default function onlyPassThroughPropsWhilePageIsVisible(BaseComponent) {
             })
           })
         }
-        console.log("Update blocked because page is not visible naja")
         return false
       } else {
         return true
