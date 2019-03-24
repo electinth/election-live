f4afdec13e702bae6b73746b04cfe9f9ac1d67ae
Chaiyapon Ob-om <chaiyapon.o@taskworld.com>
2019-03-24 10:45:54 +0700

    Improve Arrow component and add two onClick props

--- src/components/Arrow.js
+++ src/components/Arrow.js
@@ -1,16 +1,38 @@
 import React from "react"
 
-export default () => (
-  <div
-    css={{
-      display: "inline-block",
-      border: "solid #212121",
-      borderWidth: "0 2px 2px 0",
-      padding: "4px",
-      transform: "rotate(135deg)",
-      verticalAlign: "middle",
-      marginRight: "12px",
-      cursor: "pointer",
-    }}
-  />
+/**
+ * @param {{ onLeftArrowClick?: () => void, onRightArrowClick?: () => void }} props
+ */
+const Arrow = props => (
+  <React.Fragment>
+    <div
+      onClick={props.onLeftArrowClick}
+      css={{
+        display: "inline-block",
+        border: "solid #212121",
+        borderWidth: "0 2px 2px 0",
+        padding: "4px",
+        transform: "rotate(135deg)",
+        verticalAlign: "middle",
+        marginRight: "12px",
+        cursor: "pointer",
+      }}
+    />
+    {props.children}
+    <div
+      onClick={props.onRightArrowClick}
+      css={{
+        display: "inline-block",
+        border: "solid #212121",
+        borderWidth: "0 2px 2px 0",
+        padding: "4px",
+        transform: "rotate(-45deg)",
+        verticalAlign: "middle",
+        marginLeft: "12px",
+        cursor: "pointer",
+      }}
+    />
+  </React.Fragment>
 )
+
+export default Arrow
