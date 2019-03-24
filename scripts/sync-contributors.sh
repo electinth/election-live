177f7c973f5af107278413dd6aa70c62a3f8fcda
heytitle <pat.chormai@gmail.com>
2019-03-24 17:24:50 +0700

    update contributors (#173)

---
+++ scripts/sync-contributors.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SHEET_ID="1r3YDk8CAB6mtAyEJxUBPLgjW0Baqs6PWUDjcJDG5Juk"
+URL="https://docs.google.com/spreadsheets/d/$SHEET_ID/export?exportFormat=csv"
+DEST="./src/models/information/_contributors.json"
+
+echo "Syncing from $URL"
+curl $URL  \
+ | jq  -s --raw-input \ 'split("\n") | .[1:-1] 
+    | map(split(","))
+    | map({ "name": .[1], "role": .[0], "url": .[2], "github": .[3] })
+    | group_by(.role) | map({ (.[0].role): [.[]] }) | add' \
+ > $DEST
