31f4169586aa45b9de98dcf5ffe6ad5785fe1809
Unnawut Leepaisalsuwanna <unnawut@unnawut.com>
2019-03-24 13:06:45 +0700

    Desktop support for party view #157

--- src/pages/party.js
+++ src/pages/party.js
@@ -1,21 +1,15 @@
 import React from "react"
+import { DESKTOP_MIN_WIDTH, media } from "../styles"
 import MainLayout from "../components/MainLayout"
 import PerPartyView from "../components/PerPartyView"
-import PartyDropdown from "../components/PartyDropdown"
-
-import { DISPLAY_FONT } from "../styles"
+import PerPartyUnselectedView from "../components/PerPartyUnselectedView"
 
 export default ({ pageContext, location }) => {
   const partyId = pageContext.partyId
   const content = partyId ? (
     <PerPartyView partyId={partyId} />
   ) : (
-    <div css={{ padding: 20, color: "#616161", textAlign: "center" }}>
-      <h1 css={{ fontFamily: DISPLAY_FONT }}>
-        เลือกพรรคการเมือง เพื่อดูจำนวน ส.ส.
-      </h1>
-      <PartyDropdown />
-    </div>
+    <PerPartyUnselectedView />
   )
 
   return <MainLayout activeNavBarSection="by-party">{content}</MainLayout>
